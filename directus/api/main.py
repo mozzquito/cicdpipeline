04b25b928ec1f8a07f7d6668d3c845eeb9b817c7
root <root@ip-172-31-6-234.ap-southeast-1.compute.internal>
2025-02-28 09:45:40 +0700

    first push

---
+++ directus/api/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+# async def root():
+def read_root():
+    return {"message": "Hello, World!"}
+
+@app.get("/health", status_code=200) 
+def health_check():
+    return {"status": "health"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=3000)
